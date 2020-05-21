ef659d1b53c960bf9f9d069452d25a152c768c73
issaicjeon <59120849+issaicjeon@users.noreply.github.com>
2020-05-21 15:09:37 -0400

    Fixed bug

--- src/List.js
+++ src/List.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./List.css";
 import Form from "./Form.js";
 import Todo from "./Todo.js";
 import Button from "./Button.js";
--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "./button.js";
 
 export default class Todo extends React.Component {
   state = {
